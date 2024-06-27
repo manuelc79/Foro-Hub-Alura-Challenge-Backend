bd01433b7a28c5ef2732ae0876ebde1d306535b0
Ziko <manuelc79@gmail.com>
2024-06-27 15:59:45 -0300

    configurando el entorno de desarrollo

---
+++ src/main/java/foro/desafio/alura_challenge/API/Rest/AluForo/ApiRestAluForoApplication.java
@@ -0,0 +1,13 @@
+package foro.desafio.alura_challenge.API.Rest.AluForo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestAluForoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestAluForoApplication.class, args);
+	}
+
+}
